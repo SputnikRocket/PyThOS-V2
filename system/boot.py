846aa611a0df35f27f47fc96fe00de549be3e0cf
Who Am I? <108942763+SputnikRocket@users.noreply.github.com>
2022-08-22 11:17:03 -0400

    Added essential parts to boot module

---
+++ system/boot.py
@@ -0,0 +1,53 @@
+#Boot animation module for PyThOS
+
+from colorama import init, Fore, Back, Style, deinit
+from subprocess import call
+from time import sleep
+
+#Stuff to call before doing anything else
+
+
+#Main boot Icon/Frame
+def primaryFrame():
+
+    """The massive amount of empty space in here
+    has no functional purpose whatsoever, it just
+    makes it much easier to read & modify."""
+
+    init()
+    print(Fore.MAGENTA + "     ##############################################"                                                                             )
+    print(Fore.MAGENTA + "    #                                              #"                                                                            )
+    print(Fore.MAGENTA + "   #  " + Fore.RED + " PPPP       " + Fore.GREEN + " TTTTTTTTT h    " + Fore.BLUE + "   OOO    SSSSS " + Fore.MAGENTA + "  #    ")
+    print(Fore.MAGENTA + "  #   " + Fore.RED + " P   P      " + Fore.GREEN + "     T     h    " + Fore.BLUE + "  O   O  S      " + Fore.MAGENTA + "   #   ")
+    print(Fore.MAGENTA + " #    " + Fore.RED + " P   P      " + Fore.GREEN + "     T     h    " + Fore.BLUE + " O     O S      " + Fore.MAGENTA + "    #  ")
+    print(Fore.MAGENTA + "#     " + Fore.RED + " PPPP  y   y" + Fore.GREEN + "     T     hhhh " + Fore.BLUE + " O     O  SSSS  " + Fore.MAGENTA + "     # ")
+    print(Fore.MAGENTA + " #    " + Fore.RED + " P      y y " + Fore.GREEN + "     T     h   h" + Fore.BLUE + " O     O      S " + Fore.MAGENTA + "    #  ")
+    print(Fore.MAGENTA + "  #   " + Fore.RED + " P       y  " + Fore.GREEN + "     T     h   h" + Fore.BLUE + "  O   O       S " + Fore.MAGENTA + "   #   ")
+    print(Fore.MAGENTA + "   #  " + Fore.RED + " P      y   " + Fore.GREEN + "     T     h   h" + Fore.BLUE + "   OOO   SSSSS  " + Fore.MAGENTA + "  #    ")
+    print(Fore.MAGENTA + "    # " + Fore.RED + "       y    "                                   + Fore.MAGENTA +  "                                 #     ")
+    print(Fore.MAGENTA + "     ##############################################"                                                                             )
+    deinit()
+
+#Border line for booting screen
+def borderLine():
+    init()
+    print(Back.GREEN + Fore.RED + "<><><><><><><><><><><><><><><><><><><><><><><><><><><><>")
+    deinit()
+    
+bootProgress = ["*", "* *", "* * *", "* * * *", "* * * * *", "* * * * * *", "* * * * * * *", "* * * * * * * *"]
+
+#Entire boot screen run
+def bootInit():
+    init()
+    for i in range(8):
+        call("clear")
+        primaryFrame()
+        print()
+        borderLine()
+        print()
+        print(Fore.YELLOW + Back.RESET + bootProgress[i])
+        sleep(1)
+    deinit()
+
+
+    
